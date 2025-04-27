d7b87d163675900d26369ecb37663ca4abcde46d
Yaswanth <yaswanth.kumar.godina@gmail.com>
2025-04-27 04:01:28 -0400

    Finding weather by lattitude and longitude

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,10 +1,22 @@
 function showweatherDetails(event){
     event.preventDefault();
     const city=document.getElementById('city').value;
+    const lat=document.getElementById('lat').value;
+    const lon=document.getElementById('lon').value;
     const apiKey='325b4c6a22b8d9ede359bd21ac1a1fd9';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiurl1;
+    if (city){
+        apiurl1 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    }
+    else if (lat && lon) {
+        apiurl1 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    }
+    else {
+        alert("Enter City name or Lattitude and Longitude");
+        return;
+    }
 
-    fetch(apiUrl)
+    fetch(apiurl1)
     .then(response=>response.json())
     .then(data=>{
         const weatherInfo = document.getElementById('weatherInfo');
